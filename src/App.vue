c08eaae666347113c175cee78f6e6f780a6ee8e9
Kristian Larsen <kristian@greatcarrot.com>
2018-05-24 09:08:42 +0800

    made simple product lists  with data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,27 @@
 <template>
   <div id="app">
-    
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two :products="products"></product-list-two>
   </div>
 </template>
 
 <script>
+import ProductListOne from './components/ProductListOne';
+import ProductListTwo from './components/ProductListTwo';
 export default {
+  components: {
+    'product-list-one': ProductListOne,
+    ProductListTwo
+  },
   name: 'app',
   data () {
     return {
-      
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny Star', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 }
+      ]
     }
   }
 }
---
+++ src/components/ProductListOne.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in products">
+        <span>{{ product.name }}</span>
+        <span>$ {{ product.price }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['products']
+}
+</script>
+
+<style>
+
+</style>
